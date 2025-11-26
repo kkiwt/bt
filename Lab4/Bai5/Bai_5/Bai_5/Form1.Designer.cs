7a789b61f013c69c486b971d398a2b3afe7fb314
Nguyen Huu Chien <nguyenhuuchien2006@gmail.com>
2025-11-26 21:12:24 +0700

    Add 2 file Bai_5 and Bai_6.

---
+++ Lab4/Bai5/Bai_5/Bai_5/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Bai_5
+{
+    partial class Bai_5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rtbResult);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.textBoxUsername);
+            this.panel1.Controls.Add(this.textBoxURL);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(664, 267);
+            this.panel1.TabIndex = 0;
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Location = new System.Drawing.Point(4, 129);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(657, 135);
+            this.rtbResult.TabIndex = 7;
+            this.rtbResult.Text = "";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(423, 72);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(109, 51);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(146, 101);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(271, 22);
+            this.textBoxPassword.TabIndex = 5;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(146, 72);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(271, 22);
+            this.textBoxUsername.TabIndex = 4;
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(146, 44);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(386, 22);
+            this.textBoxURL.TabIndex = 3;
+            this.textBoxURL.TabStop = false;
+            this.textBoxURL.Text = "https://nt106.uitiot.vn/auth/token";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // Bai_5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 291);
+            this.Controls.Add(this.panel1);
+            this.Name = "Bai_5";
+            this.Text = "Bai_5";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.RichTextBox rtbResult;
+    }
+}
+
---
+++ Lab4/Bai6/Bai_6/Bai_6/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Bai_6
+{
+    partial class Bai_6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxToken = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rtbResult);
+            this.panel1.Controls.Add(this.btnGet);
+            this.panel1.Controls.Add(this.textBoxToken);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBoxURL);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(663, 266);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(146, 44);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(386, 22);
+            this.textBoxURL.TabIndex = 5;
+            this.textBoxURL.TabStop = false;
+            this.textBoxURL.Text = "https://nt106.uitiot.vn/auth/token";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL";
+            // 
+            // textBoxToken
+            // 
+            this.textBoxToken.Location = new System.Drawing.Point(146, 72);
+            this.textBoxToken.Multiline = true;
+            this.textBoxToken.Name = "textBoxToken";
+            this.textBoxToken.Size = new System.Drawing.Size(386, 70);
+            this.textBoxToken.TabIndex = 7;
+            this.textBoxToken.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Token";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGet.Location = new System.Drawing.Point(539, 42);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 52);
+            this.btnGet.TabIndex = 8;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Location = new System.Drawing.Point(4, 148);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(656, 115);
+            this.rtbResult.TabIndex = 9;
+            this.rtbResult.Text = "";
+            // 
+            // Bai_6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 291);
+            this.Controls.Add(this.panel1);
+            this.Name = "Bai_6";
+            this.Text = "Bai_6";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.TextBox textBoxToken;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
