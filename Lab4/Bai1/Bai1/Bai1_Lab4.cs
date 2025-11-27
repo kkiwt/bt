9b102fc2fd21188355817fc711daf12ff53e1089
Thanh Nguyễn Nhật <24521633@ms.uit.edu.vn>
2025-11-27 22:58:05 +0700

    Bai01_Lab04

---
+++ Lab4/Bai1/Bai1/Bai1_Lab4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1
+{
+    public partial class Bai1_Lab4 : Form
+    {
+        public Bai1_Lab4()
+        {
+            InitializeComponent();
+        }
+
+        private string getHTML(string szUrl)
+        {
+            WebRequest request = WebRequest.Create(szUrl); //Tao yeu cau den URL
+            WebResponse response = request.GetResponse(); //Nhan phan hoi
+
+            Stream dataStream = response.GetResponseStream(); //Don nhan Stream
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd(); //Doc toan bo ndung tu Stream
+
+            response.Close(); //Dong phan hoi
+            return responseFromServer; //tra ve ndung tu sv
+        }
+
+        private void Get_Click(object sender, EventArgs e)
+        {
+            string WebHTML = "";
+            WebHTML = getHTML(URL.Text);
+            NoiDung.Text = WebHTML;
+        }
+    }
+}
